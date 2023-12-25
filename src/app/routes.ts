3d7b68008886236a6459c3d71c6f5e06151d7b96
Nicholas Liem <13521135@std.stei.itb.ac.id>
2023-12-25 22:44:20 +0700

    feat: adding rate limitter

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -4,9 +4,11 @@ import { authRoutes } from './adapters/express/routes/auth.route'
 import healthRoutes from './adapters/express/routes/health.route'
 import { UserController } from './application/controllers/user.controller'
 import { type ServiceContainer } from './containers/service.container'
+import generalRateLimiter from './adapters/express/middlewares/limiter/general_limiter.middleware'
 
 export function routes (app: Express, container: ServiceContainer) {
     const userController = new UserController(container.getUserService())
+    app.use(generalRateLimiter)
     app.use('/api/v1/health', healthRoutes)
     app.use('/api/v1/users', userRoutes(userController))
     app.use('/api/v1/auth', authRoutes(userController))
