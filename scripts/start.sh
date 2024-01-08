14f4e0f26fe33ad5b7a16d2be49fbddca581d137
Nicholas Liem <nicholas.liem@shippit.com>
2024-01-08 22:11:13 +0700

    feat: fix dockerfile steps and update starting script, add node_modules to volume

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,12 @@
 echo "[SCRIPTS] Building the server.."
 
+# Change directory to src
+cd src
+
+# Run Prisma Generate
+echo "[SCRIPTS] Making Prisma Files..."
+npx prisma generate
+
 # Run Prisma migrations
 echo "[SCRIPTS] Running Prisma migrations..."
 npx prisma migrate deploy
